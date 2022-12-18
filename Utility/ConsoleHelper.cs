1acafe0ecadfb990f669e7ad5f7893c6beb08e9a
Panayot Ivanov <panayot_zhi@abv.bg>
2022-12-18 15:31:51 +0200

    Add helpers.

---
+++ Utility/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿namespace WarOfRightsUnpack.Utility
+{
+    public static class ConsoleHelper
+    {
+        public static void WriteLine()
+        {
+            Console.WriteLine(string.Empty);
+        }
+
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void WriteWarning(string message)
+        {
+            Write(message, null, ConsoleColor.Yellow);
+        }
+
+        public static void WriteError(string message)
+        {
+            Write(message, null, ConsoleColor.DarkRed);
+        }
+
+        private static void Write(string message, ConsoleColor? newBackground, ConsoleColor? newForeground)
+        {
+            // save original colors
+            var background = Console.BackgroundColor;
+            var foreground = Console.ForegroundColor;
+
+            //Console.Write(Environment.NewLine);
+
+            if (newBackground.HasValue)
+            {
+                Console.BackgroundColor = newBackground.Value;
+            }
+
+            if (newForeground.HasValue)
+            {
+                Console.ForegroundColor = newForeground.Value;
+            }
+
+            Console.WriteLine(message);
+
+            // restore original colors
+            Console.BackgroundColor = background;
+            Console.ForegroundColor = foreground;
+        }
+    }
+}
---
+++ Utility/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace WarOfRightsUnpack.Utility
+{
+    public static class FileHelper
+    {
+        public static FileInfo GetFile(DirectoryInfo directoryInfo, string fileName)
+        {
+            var results = directoryInfo.GetFiles(fileName, SearchOption.AllDirectories).ToArray();
+            if (results.Length > 1)
+            {
+                Console.WriteLine($"Found more than one '\"{fileName}\"' file within the specified directory '{directoryInfo.FullName}'.");
+                Console.WriteLine("Taking the first occurrence an proceeding...");
+            }
+            else if (results.Length == 0)
+            {
+                throw new ArgumentNullException(
+                    message: $"Cannot find '\"{fileName}\"' file within the specified directory '{directoryInfo.FullName}'.",
+                    paramName: nameof(results));
+            }
+
+            return results.First();
+        }
+    }
+}
