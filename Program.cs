6f9f9572d5be9dc36e0bddeb5691459b75b6f93a
Panayot Ivanov <panayot_zhi@abv.bg>
2022-12-18 15:32:21 +0200

    Wire main's.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using CommandLine;
+using System.Globalization;
+using WarOfRightsUnpack.Main;
+
+Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+Parser.Default.ParseArguments<
+        UnpackGameAssets.Options,
+        ExtractMaps.Options,
+        ExtractWeapons.Options,
+        ExtractRegiments.Options,
+        ExtractRegimentsImages.Options,
+        ExtractMisc.Options
+    >(args)
+    .WithParsed<UnpackGameAssets.Options>(UnpackGameAssets.Run)
+    .WithParsed<ExtractMaps.Options>(ExtractMaps.Run)
+    .WithParsed<ExtractWeapons.Options>(ExtractWeapons.Run)
+    .WithParsed<ExtractRegiments.Options>(ExtractRegiments.Run)
+    .WithParsed<ExtractRegimentsImages.Options>(ExtractRegimentsImages.Run)
+    .WithParsed<ExtractMisc.Options>(ExtractMisc.Run);
+
+Console.WriteLine();
+Console.WriteLine("Done.");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
